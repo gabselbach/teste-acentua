8a5b545d333623afc011ccfea2a93a5843355307
Gabriella Selbach <gabriellaselbach@gmail.com>
2020-06-05 09:43:56 -0300

    nova rota

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -9,6 +9,10 @@ cors = CORS(app, resource={r"/*":{"origins": "*"}})
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/novo')
+def hello_world():
+    return 'Ola mundo cruel!'
+
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
