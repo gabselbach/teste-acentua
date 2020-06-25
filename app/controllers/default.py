0070f178a5863111991c256183ed9651cf7d3010
Gabriella Selbach <gabriellaselbach@gmail.com>
2020-06-25 13:57:14 -0300

    trocando requiriments

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -23,10 +23,3 @@ def teste():
 def novo():
     #form = TextoForm()
     return render_template('index.html')
-
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    main()
--- run.py
+++ run.py
@@ -1,4 +1,15 @@
+import os
 from app import app
+from flask_cors import CORS
+
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
-    app.run()
+    main()
+#if __name__ == "__main__":
+#   app.run()
